0f16e9ee8ae1996353ec8674e55c1b819150bb79
BadrChoubai <badr.choubai1048@gmail.com>
2018-04-17 23:27:05 -0600

    little changes

--- doubleNumbers.js
+++ doubleNumbers.js
@@ -3,5 +3,6 @@ let doubleNumbers = (x, y, z) => {
     let arrayToDouble = array.map(number => number * 2);
     return arrayToDouble;
 };
+
 let args = [3, 2, 2];
 console.log(doubleNumbers(...args));
--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,4 @@
 const reverseString  = (str) => {
-    var originalStr = str;
-    console.log(originalStr);
     return str.split('').reverse().join('');
 };
 console.log(reverseString("hello"));
