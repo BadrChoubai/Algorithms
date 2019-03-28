ca56757dd12dec82628994fe210579d03d402786
Badr Choubai <badr.choubaidev@outlook.com>
2019-03-28 14:54:02 -0600

    refactor multiplyAllNotMe

--- JS/multiplyAllNotMe.js
+++ JS/multiplyAllNotMe.js
@@ -1,17 +1,17 @@
-const productFromArray = (arr) => arr.reduce((a, b) => a * b);
+const productOfArray = (arr) => arr.reduce((a, b) => a * b);
 
 function multiplyallNotMe(arr) {
-  let productOfArray = productFromArray(arr);
-  let productArray = [];
-  arr.forEach(num => productArray.push(productOfArray / num));
+  let product = productOfArray(arr);
+  let result = new Array(arr.length);
 
-  return productArray;
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = (product / arr[i]);
+  }
+
+  return result;
 };
 
-
-console.log(multiplyallNotMe([1, 2, 3, 4, 5]));
-console.log(multiplyallNotMe([3, 2, 1]));
-
+console.log(multiplyallNotMe([2, 4, 6, 8, 10]));
 
 /*
 
@@ -19,6 +19,6 @@ Given an array of integers, return a new array such that each element at index i
 
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
-Follow-up: What if you can't use division?
+Optional: What if you can't use division?
 
 */
