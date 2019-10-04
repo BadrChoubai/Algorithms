b342075a78fd72a88e6c760a5828958fc59c41db
Badr Choubai <badr.choubaidev@outlook.com>
2019-10-03 18:44:48 -0600

    add chap3-challenge minimum

---
+++ JavaScript/EloquentJS/Chap3-Challenges/minimum.js
@@ -0,0 +1,13 @@
+/**
+ * @function minimum
+ * @param {Number} numOne
+ * @param {Number} numTwo
+ * @description
+ * Write a function similar to Math.min that takes
+ * two numbers and return the smaller one.
+ */
+function minimum(numOne, numTwo) {
+  return numOne < numTwo 
+    ? numOne 
+    : numTwo;
+}
