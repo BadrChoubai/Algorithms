82583b924c0cd5a0ac60c39bc9c85e9ca6c39c8b
Badr Choubai <badr.choubaidev@outlook.com>
2019-10-06 11:03:41 -0600

    refactor solution

--- JavaScript/codewars/6kyu/spinWords/index.js
+++ JavaScript/codewars/6kyu/spinWords/index.js
@@ -6,17 +6,11 @@
  * but with all five or more letter words reversed
  */
 function spinWords(sentence) {
-  let splitSentence = input => input.split(' ');
-  let wordArray = splitSentence(sentence);
-
-  return wordArray.map(word => {
+  return sentence.split(' ').map(word => {
     return word.length >= 5 ? word.split('').reverse().join('') : word;
   }).join(' ');
-
 };
 
-let test1 = spinWords("I love JavaScript String manipulation");
-let test2 = spinWords("Wandering around wondering what's going on");
-let test3 = spinWords("Sleep the day away");
-
-console.log(test1, test2, test3);
+spinWords("I love JavaScript String manipulation");
+spinWords("Wandering around wondering what's going on");
+spinWords("Sleep the day away");
