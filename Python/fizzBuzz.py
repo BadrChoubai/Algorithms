4e2098531db4f02f0dd47c1dd39c34c81da17870
Badr Choubai <badr.choubaidev@outlook.com>
2019-10-22 09:34:58 -0600

    change fizzbuzz

--- Python/fizzBuzz.py
+++ Python/fizzBuzz.py
@@ -3,9 +3,9 @@ def fizzBuzz():
     if(i % 15 == 0):
       print("FizzBuzz")
     elif (i % 3 == 0):
-      print("Buzz")
-    elif (i % 5 == 0):
       print("Fizz")
+    elif (i % 5 == 0):
+      print("Buzz")
     else:
       print(i)
 
